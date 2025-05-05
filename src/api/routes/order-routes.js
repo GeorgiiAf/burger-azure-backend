64783686bb85d3b9caf5c4f223c331a26fdd7f27
axerex12 <axel.aaldijk12@gmail.com>
2025-05-05 17:32:40 +0300

    Add getAllReservationProducts functionality and route

--- src/api/routes/order-routes.js
+++ src/api/routes/order-routes.js
@@ -3,6 +3,7 @@ import {
   createNewOrder,
   getOrder,
   getAllOrders,
+  getAllReservationProductsController
 } from '../controllers/order-controller.js';
 
 const router = express.Router();
@@ -12,5 +13,7 @@ router.route('/orders').post(createNewOrder).get(getAllOrders);
 
 // Route for fetching a specific order by ID
 router.get('/orders/:id', getOrder);
+
+router.get('/reservation-products', getAllReservationProductsController);
 
 export default router;
